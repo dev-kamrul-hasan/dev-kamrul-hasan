d2c8635ba205f1ba1a214af998f16515ea731cb6
dev-kamrul-hasan <minecraftalias12@gmail.com>
2026-03-27 11:10:44 +0600

    First official push

---
+++ 01-hello-world.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    int year = 2026;
+    printf("Hello, World. This is Akash.\n");
+    printf("The year is %d, and my coding journey start now.\n", year);
+
+    return 0;
+}
